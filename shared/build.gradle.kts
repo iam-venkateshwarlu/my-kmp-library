017b60a42de9fc33b4a3f87193d706f9e0bac467
iam-venkateshwarlu <tvenkateshwarlu39@gmail.com>
2026-05-20 15:44:57 +0530

    Fix gradlew permission in workflow

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -5,13 +5,14 @@ plugins {
     alias(libs.plugins.kotlinSerialization)
 
     id("com.vanniktech.maven.publish") version "0.30.0"
-    signing
+    id("signing")
 }
 
 group = "io.github.iam-venkateshwarlu"
 version = "1.0.0"
 
 kotlin {
+
     androidTarget {
         compilations.all {
             kotlinOptions {
@@ -19,38 +20,43 @@ kotlin {
             }
         }
     }
-    
+
     listOf(
         iosX64(),
         iosArm64(),
         iosSimulatorArm64()
     ).forEach { iosTarget ->
+
         iosTarget.binaries.framework {
             baseName = "shared"
             isStatic = true
         }
     }
-    
+
     sourceSets {
+
         commonMain.dependencies {
+
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material3)
+
             @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
             implementation(compose.components.resources)
-            
+
             implementation(libs.ktor.client.core)
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.serialization.kotlinx.json)
             implementation(libs.kotlinx.serialization.json)
+
             implementation(libs.koin.core)
             implementation(libs.lifecycle.viewmodel.compose)
         }
-        
+
         androidMain.dependencies {
             implementation(libs.ktor.client.android)
         }
-        
+
         iosMain.dependencies {
             implementation(libs.ktor.client.darwin)
         }
@@ -58,13 +64,84 @@ kotlin {
 }
 
 android {
+
     namespace = "com.example.cryptoapp.shared"
+
     compileSdk = 34
+
     defaultConfig {
         minSdk = 24
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+}
+
+mavenPublishing {
+
+    publishToMavenCentral()
+
+    signAllPublications()
+
+    pom {
+
+        name.set("CryptoApp")
+
+        description.set(
+            "Kotlin Multiplatform crypto library"
+        )
+
+        inceptionYear.set("2026")
+
+        url.set(
+            "https://github.com/iam-venkateshwarlu/my-kmp-library"
+        )
+
+        licenses {
+
+            license {
+
+                name.set(
+                    "The Apache License, Version 2.0"
+                )
+
+                url.set(
+                    "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                )
+            }
+        }
+
+        developers {
+
+            developer {
+
+                id.set("iam-venkateshwarlu")
+
+                name.set("Venkateshwarlu")
+
+                url.set(
+                    "https://github.com/iam-venkateshwarlu"
+                )
+            }
+        }
+
+        scm {
+
+            url.set(
+                "https://github.com/iam-venkateshwarlu/my-kmp-library"
+            )
+
+            connection.set(
+                "scm:git:git://github.com/iam-venkateshwarlu/my-kmp-library.git"
+            )
+
+            developerConnection.set(
+                "scm:git:ssh://git@github.com/iam-venkateshwarlu/my-kmp-library.git"
+            )
+        }
+    }
+
+    // javadocJar()
 }
